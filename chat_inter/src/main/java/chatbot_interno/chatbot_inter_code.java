350d94222c4a3629f0a57d4312f925f391bd8c1a
l231080580-alejandrobri <l231080580@iztapalapa.tecnm.mx>
2025-11-24 23:17:40 -0600

    Add files via upload

---
+++ chat_inter/src/main/java/chatbot_interno/chatbot_inter_code.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package chatbot_interno;
+
+/**
+ *
+ * @author Dell
+ */
+public class chatbot_inter_code {
+    
+}
